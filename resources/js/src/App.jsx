6c1ad4e3f9832531b6d51557ef43f543ca8fe187
JoudevCoy <joudev09@gmail.com>
2024-09-04 22:16:43 +0800

    ini FE nya #joudev

---
+++ resources/js/src/App.jsx
@@ -0,0 +1,12 @@
+import LoginPage from "./components/LoginPage"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <LoginPage />
+    </>
+  )
+}
+
+export default App
---
+++ resources/js/src/components/LoginPage.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState, useRef } from "react"
+
+export default function LoginPage() {
+  return (
+    <>
+      <h1 className="text-2xl text-center font-black">Hello, World!</h1>
+    </>
+  )
+}
